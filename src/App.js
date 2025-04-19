51bb7fa8ae56edfd3b0bb321ac1dd22c98e33c37
Tarık İslamoğlu <tarikislamoglu8@gmail.com>
2025-04-19 10:54:46 +0300

    updated3

--- src/App.js
+++ src/App.js
@@ -79,10 +79,7 @@ export default function InviteUsers() {
             id="email"
             className={`px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 ${
               state.error && "animate-pulse"
-            } ${
-              state.success &&
-              "transtion-all animate-bounce  duration-200 ease-in-out"
-            }`}
+            } ${state.success && " animate-bounce "}`}
             placeholder="E-posta giriniz"
             onChange={(e) => setInputValue(e.target.value)}
             value={inputValue}
